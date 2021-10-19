c5977da020b130f1991f4f95ef1bce68fba6b573
Denis Petrov-Ploskirev <todenis@inbox.ru>
2021-10-19 11:48:55 +0300

    Code and style fixies

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -17,10 +17,10 @@ let loaderTimeout;
  * Функция задаёт первоначальное состояние страницы.
  * Отправляется первый запрос за картинками, юез параметров т.к. с дефолтными настройками.
  */
-const initialState = function () {
+const initialState = function() {
     action.disabled = false;
     getPictures();
-}
+};
 
 /**
  * Функция запрашивает картинки для галереи
@@ -28,43 +28,51 @@ const initialState = function () {
  * @param {number} page
  * @param {number} limit
  */
-const getPictures = function (page = 1, limit = 10) {
+const getPictures = function(page = 1, limit = 10) {
     showLoader();
     fetch(`https://picsum.photos/v2/list?page=${page};limit=${limit}`)
-        .then(function (response) {return response.json()})
-        .then(function (result) {renderPictures(result)})
-}
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(result) {
+            renderPictures(result);
+        });
+};
 
 /**
  * Функция запрашивает информацию о конкретной картинке по её id
  * и вызывает ф-цию для отрисовки картинки в попапе
  * @param {number} id
  */
-const getPictureInfo = function (id = 0) {
+const getPictureInfo = function(id = 0) {
     showLoader();
     fetch(`https://picsum.photos/id/${id}/info`)
-        .then(function (response) {return response.json()})
-        .then(function (result) {renderPopupPicture(result)})
-}
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(result) {
+            renderPopupPicture(result);
+        });
+};
 
 /**
  * Функция показывает индикатор загрузки.
  * Меняет ситили, ничего не возвращает.
  */
-const showLoader = function () {
+const showLoader = function() {
     loader.style.visibility = 'visible';
-}
+};
 
 /**
  * Функция скрывает индикатор загрузки.
  * Удаляет таймаут индикатора, ничего не возвращает.
  */
-const hideLoader = function () {
-    loaderTimeout = setTimeout(function () {
+const hideLoader = function() {
+    loaderTimeout = setTimeout(function() {
         loader.style.visibility = 'hidden';
-        loaderTimeout.clearTimeout();
+        clearTimeout(loaderTimeout);
     }, 700);
-}
+};
 
 /**
  * Функция пропорционально делит размер картинки,
@@ -73,30 +81,29 @@ const hideLoader = function () {
  * @param {string} src
  * @param {number} size
  */
-const cropImage = function (src, size = 2) {
+const cropImage = function(src, size = 2) {
     const [domain, key, id, width, height] = src.split('/').splice(2);
     const newWidth = Math.floor(+width / size);
     const newHeight = Math.floor(+height / size);
 
     return `https://${domain}/${key}/${id}/${newWidth}/${newHeight}`;
-}
+};
 
 /**
  * Функция копирует шаблон для каждой картинки,
  * заполняет его и встраивает в разметку
  * @param {array} list
  */
-const renderPictures = function (list) {
+const renderPictures = function(list) {
     if (!list.length) {
         throw Error(`Pictures not defined. The list length: ${list.length}`);
     }
 
-    const clone = templateImageCard.content.cloneNode(true);
     const fragment = document.createDocumentFragment();
 
-    list.forEach(function (element) {
+    list.forEach(function(element) {
+        const clone = templateImageCard.content.cloneNode(true);
         const link = clone.querySelector('a');
-
         link.href = element.url;
         link.dataset.id = element.id;
 
@@ -104,19 +111,19 @@ const renderPictures = function (list) {
         image.src = cropImage(element.download_url, 5);
         image.alt = element.author;
         image.classList.add('preview');
-        fragment.appendChild(clone)
+        fragment.appendChild(clone);
     });
 
     container.appendChild(fragment);
     hideLoader();
-}
+};
 
 /**
  * Функция копирует шаблон для картинки в попапе,
  * заполняет его и встраивает в попап
  * @param {object} picture
  */
-const renderPopupPicture = function (picture) {
+const renderPopupPicture = function(picture) {
     const clone = templateImagePopup.content.cloneNode(true);
     const img = clone.querySelector('img');
     const link = clone.querySelector('a');
@@ -129,17 +136,17 @@ const renderPopupPicture = function (picture) {
     link.href = picture.download_url;
 
     popupContainer.innerHTML = '';
-    popupContainer.appendChild(clone)
+    popupContainer.appendChild(clone);
     hideLoader();
     togglePopup();
-}
+};
 
 /**
  * Функция переклбчает класс открытия на попапе
  */
-const togglePopup = function () {
+const togglePopup = function() {
     popup.classList.toggle('open');
-}
+};
 
 /**
  * @type {object} MouseEvent
@@ -149,18 +156,20 @@ const togglePopup = function () {
  * Обработчик кнопки подгрузки картинок
  * @param {MouseEvent} evt
  */
-const actionHandler = function (evt) {
+const actionHandler = function(evt) {
     evt.preventDefault();
-    const nextPage = evt.currentTarget.dataset.page;
+    const nextPage = +evt.currentTarget.dataset.page;
     evt.currentTarget.dataset.page = nextPage + 1;
 
     if (nextPage > MAX_PAGE_IAMGES) {
-        console.warn(`WARN: You are trying to call a page that exceeds ${MAX_PAGE_IAMGES}`);
+        console.warn(
+            `WARN: You are trying to call a page that exceeds ${MAX_PAGE_IAMGES}`
+        );
         evt.currentTarget.disabled = true;
     } else {
         getPictures(nextPage);
     }
-}
+};
 
 /**
  * Обработчик события click по картинкам.
@@ -168,13 +177,13 @@ const actionHandler = function (evt) {
  * для открытия попапа с ней
  * @param {MouseEvent} evt
  */
-const imageHandler = function (evt) {
+const imageHandler = function(evt) {
     evt.preventDefault();
 
     if (evt.target.closest('a')) {
-        getPictureInfo(evt.target.dataset.id);
+        getPictureInfo(evt.target.closest('a').dataset.id);
     }
-}
+};
 
 action.addEventListener('click', actionHandler);
 container.addEventListener('click', imageHandler);
